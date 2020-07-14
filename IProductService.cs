08a805d56983d9029a5fc60374f162d23e4ea80d
rajeshrpec <68283518+rajeshrpec@users.noreply.github.com>
2020-07-14 22:24:01 +0530

    Update IProductService.cs

--- IProductService.cs
+++ IProductService.cs
@@ -2,6 +2,7 @@ namespace PromotionEngine
 {
     public interface IProductService
     {
+        //Interface method which calculates the final price once promotion applied for selected items.
         int GetTotalPrice(List<Product> products);
     }
 }
