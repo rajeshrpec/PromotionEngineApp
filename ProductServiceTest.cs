f9918307e067b586a03dfb33453d69ee945e03f4
rajeshrpec <68283518+rajeshrpec@users.noreply.github.com>
2020-07-14 22:20:40 +0530

    Update ProductServiceTest.cs

--- ProductServiceTest.cs
+++ ProductServiceTest.cs
@@ -11,20 +11,24 @@ namespace UnitTestProject
         [TestMethod]
         public void ScenarioA_Test()
         {
+            //Initialize
              List<Product> product =new List<Product>();
             ProductService.Add(new Product(){Id="A"});
             ProductService.Add(new Product(){Id="B"});
             ProductService.Add(new Product(){Id="C"});
-
+            
+            //Action
             ProductService productService=new ProductService();
             int totalPrice= productService.GetTotalPrice(products);
-
+            
+            //Assert
             Assert.AreEqual<int>(100,totalPrice);
         }
 
         [TestMethod]
         public void ScenarioB_Test()
         {
+            //Initialize
             List<Product> product =new List<Product>();
             ProductService.Add(new Product(){Id="A"});
             ProductService.Add(new Product(){Id="A"});
@@ -37,16 +41,19 @@ namespace UnitTestProject
             ProductService.Add(new Product(){Id="B"});
             ProductService.Add(new Product(){Id="B"});
             ProductService.Add(new Product(){Id="C"});
-
+            
+             //Action
             ProductService productService=new ProductService();
             int totalPrice= productService.GetTotalPrice(products);
-
+            
+            //Assert
             Assert.AreEqual<int>(370,totalPrice);
         }
 
         [TestMethod]
         public void ScenarioC_Test()
         {
+            //Initialize
             List<Product> product =new List<Product>();
             ProductService.Add(new Product(){Id="A"});
             ProductService.Add(new Product(){Id="A"});
@@ -58,10 +65,25 @@ namespace UnitTestProject
             ProductService.Add(new Product(){Id="B"});
             ProductService.Add(new Product(){Id="C"});
             ProductService.Add(new Product(){Id="D"});
-
+            
+            //Action
             ProductService productService=new ProductService();
             int totalPrice= productService.GetTotalPrice(products);
-
+            
+            //Assert
+            Assert.AreEqual<int>(280,totalPrice);
+        }
+        [TestMethod]
+        public void NoProductsScenario_Test()
+        {
+            //Initialize
+            List<Product> product =new List<Product>();
+            
+            //Action
+            ProductService productService=new ProductService();
+            int totalPrice= productService.GetTotalPrice(products);
+            
+            //Assert
             Assert.AreEqual<int>(280,totalPrice);
         }
     }
