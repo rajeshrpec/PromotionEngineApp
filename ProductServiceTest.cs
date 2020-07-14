0e16ba258451d136c4b594cbd9c9610e4c0cb610
rajeshrpec <68283518+rajeshrpec@users.noreply.github.com>
2020-07-14 21:23:23 +0530

    Create ProductServiceTest.cs

---
+++ ProductServiceTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using PromotionEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTesting
+    {
+        [TestMethod]
+        public void ScenarioA_Test()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ScenarioB_Test()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ScenarioC_Test()
+        {
+            
+        }
+    }
+}
