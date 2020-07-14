c651a827f723712c1db72c9085c4360c68c4dc27
rajeshrpec <68283518+rajeshrpec@users.noreply.github.com>
2020-07-14 21:51:15 +0530

    Update ProductService.cs

--- ProductService.cs
+++ ProductService.cs
@@ -5,7 +5,31 @@ namespace PromotionEngine
     
         public int GetTotalPrice( List<Products> products)
         {
-                
+            int counterA=0;
+            int counterB=0;
+            int counterC=0;
+            int counterD=0;
+            
+            foreach (Product product in products)
+            {
+                if(product.Id=="A" || product.Id=="a")
+                {
+                    counterA=counterA+1;
+                }
+                if(product.Id=="B" || product.Id=="b")
+                {
+                    counterB=counterB+1;
+                }
+                if(product.Id=="C" || product.Id=="c")
+                {
+                    counterC=counterC+1;
+                }
+                if(product.Id=="D" || product.Id=="d")
+                {
+                    counterD=counterD+1;
+                }
+
+            }
         }
     
     }
