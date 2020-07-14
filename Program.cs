b3d803985637e8bb7924a0036eecfff694d88225
rajeshrpec <68283518+rajeshrpec@users.noreply.github.com>
2020-07-14 21:45:37 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,18 @@ namespace PromotionEngine
         console.WriteLine("Total no of Orders");
         int NoOfOrders = convert.ToInt32(console.readLine());
         
+        for (int i = 0; i < NoOfOrders; i++)
+        {
+            console.WriteLine("Please provide the type of product: A or B or C or D");
+            string productType=console.readLine();
+            Product product=new Product();
+            product.Id=productType;
+            products.Add(product);
+        }
+        
+        int totalPrice= productService.GetTotalPrice(products);
+        console.WriteLine(totalPrice);
+        console.readLine();
     }
     
  }
