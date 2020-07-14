dd5f3b164938f47263ce007dec96d2c23943ecf7
rajeshrpec <68283518+rajeshrpec@users.noreply.github.com>
2020-07-14 21:43:33 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace PromotionEngine
     static void Main(string[] args)
     {
     
+        List<Product> products =new List<Product>();
+        ProductService productService = new ProductService();
+
+        console.WriteLine("Total no of Orders");
+        int NoOfOrders = convert.ToInt32(console.readLine());
+        
     }
     
  }
